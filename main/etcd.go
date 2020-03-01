71035ca4e57fd686964241fe6b9ac7c8bdb2aedd
JustDoIt <595265578@qq.com>
2020-03-02 00:12:08 +0800

    20200301

--- main/etcd.go
+++ main/etcd.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/astaxie/beego/logs"
 	"go.etcd.io/etcd/clientv3"
 	"kafka/tailf"
@@ -10,15 +13,15 @@ import (
 
 type EtcdClient struct {
 	client *clientv3.Client
-	kyes []string
+	keys   []string
 }
 
 var (
 	etcdClient *EtcdClient
 )
 
-func initEtcd(addr ,key string) (collectConf []tailf.CollectConf,err error) {
-	cli,err := clientv3.New(clientv3.Config{
+func initEtcd(addr, key string) (collectConf []tailf.CollectConf, err error) {
+	cli, err := clientv3.New(clientv3.Config{
 		Endpoints:            []string{addr},
 		AutoSyncInterval:     0,
 		DialTimeout:          5 * time.Second,
@@ -43,14 +46,51 @@ func initEtcd(addr ,key string) (collectConf []tailf.CollectConf,err error) {
 
 	etcdClient = &EtcdClient{
 		client: cli,
-		kyes:   nil,
+		keys:   nil,
 	}
 
-	if strings.HasPrefix(key,"/") == false {
+	if strings.HasPrefix(key, "/") == false {
 		key = key + "/"
 	}
 
+	for _, ip := range localIPArray {
+		etcdKey := fmt.Sprintf("%s%s", key, ip)
+		etcdClient.keys = append(etcdClient.keys, etcdKey)
 
+		ctx, cancle := context.WithTimeout(context.Background(), time.Second)
 
+		resp, err := cli.Get(ctx, etcdKey)
+		if err != nil {
+			logs.Error("client get from etcd failed, err:%v", err)
+			continue
+		}
+		cancle()
+
+		logs.Debug("resp from etcd_conf:%v", resp.Kvs)
+
+		for _, v := range resp.Kvs {
+			if string(v.Key) == etcdKey {
+				err = json.Unmarshal(v.Value, &collectConf)
+				if err != nil {
+					logs.Error("unmarshal failed, err:%v", err)
+					continue
+				}
+
+				logs.Debug("log config is %v", collectConf)
+			}
+		}
+	}
+	initEtcdWatcher()
 	return
+}
+
+func initEtcdWatcher() {
+
+	for _, key := range etcdClient.keys {
+		go wathcKey(key)
+	}
+}
+
+func wathcKey(key string) {
+
 }
